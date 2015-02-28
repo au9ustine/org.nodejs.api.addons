300814aa835cbcdccb60ff0e6ef415d1c6a8b381
au9ustine <duke.augustine@gmail.com>
2015-02-28 15:56:14 +0800

    Add prototype of app

--- hello.cc
+++ hello.cc
@@ -1,3 +1,5 @@
+// -*- coding: utf-8 -*-
+
 // hello.cc
 #include <node.h>
 
@@ -6,7 +8,7 @@ using namespace v8;
 void Method(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = Isolate::GetCurrent();
   HandleScope scope(isolate);
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "世界"));
 }
 
 void init(Handle<Object> exports) {
@@ -14,3 +16,7 @@ void init(Handle<Object> exports) {
 }
 
 NODE_MODULE(addon, init)
+
+// [au9ustine]: at least node addons could provide another way to build
+//              c++ modules apart from python/c++ interop and JNI, and 
+//              it's quite easy :D
